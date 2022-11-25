f6d74314cbf2fe3028ce72285d542205db71c364
djtom <davisjacobthomas@gmail.com>
2022-11-25 17:22:07 +0100

    modified init

--- src/classfunc/classfunc/transf.py
+++ src/classfunc/classfunc/transf.py
@@ -1,29 +1,30 @@
 
 from abc import ABC, abstractmethod
 import numpy as np
+testdata=[]
 # from sklearn.preprocessing import StandardScaler 
 class feature_transform(ABC):
     
-    @abstractmethod
-    def apply_transform(self):
-        return NotImplementedError
+        @abstractmethod
+        def apply_transform(self):
+            return NotImplementedError
 
 class log_transform(feature_transform):
 
-    def __init__(self,df,cols:list) -> None:
-         self.df=df
-         self.cols=cols
+        def __init__(self,df,cols:list) -> None:
+            self.df=df
+            self.cols=cols
 
-    def apply_transform(self):
-        self.df.loc[:,self.cols]=self.df.loc[:,self.cols].apply(lambda x: np.log(x))
+        def apply_transform(self):
+            self.df.loc[:,self.cols]=self.df.loc[:,self.cols].apply(lambda x: np.log(x))
     
 class reverse_log(feature_transform):
 
-    def __init__(self,df,cols:list) -> None:
-         self.df=df
-         self.cols=cols
-         
-    def apply_transform(self):
-        # obj=StandardScaler()
-        self.df.loc[:,self.cols]=self.df.loc[:,self.cols].apply(lambda x: np.exp(x))
+        def __init__(self,df,cols:list) -> None:
+            self.df=df
+            self.cols=cols
+            
+        def apply_transform(self):
+            # obj=StandardScaler()
+            self.df.loc[:,self.cols]=self.df.loc[:,self.cols].apply(lambda x: np.exp(x))
 
