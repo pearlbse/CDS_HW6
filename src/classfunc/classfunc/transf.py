3f3b46b505dc38922ef3189e0a368a9bdf9d583c
djtom <davisjacobthomas@gmail.com>
2022-11-25 17:46:24 +0100

    modified code for transf

--- src/classfunc/classfunc/transf.py
+++ src/classfunc/classfunc/transf.py
@@ -4,27 +4,22 @@ import numpy as np
 testdata=[]
 # from sklearn.preprocessing import StandardScaler 
 class feature_transform(ABC):
-    
+
+        def __init__(self,df,cols:list) -> None:
+            self.df=df
+            self.cols=cols
+
         @abstractmethod
         def apply_transform(self):
             return NotImplementedError
 
 class log_transform(feature_transform):
 
-        def __init__(self,df,cols:list) -> None:
-            self.df=df
-            self.cols=cols
-
         def apply_transform(self):
             self.df.loc[:,self.cols]=self.df.loc[:,self.cols].apply(lambda x: np.log(x))
     
 class reverse_log(feature_transform):
 
-        def __init__(self,df,cols:list) -> None:
-            self.df=df
-            self.cols=cols
-            
         def apply_transform(self):
-            # obj=StandardScaler()
             self.df.loc[:,self.cols]=self.df.loc[:,self.cols].apply(lambda x: np.exp(x))
 
